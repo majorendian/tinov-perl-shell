eb074c3a0ecd24c2bc7d3b0793f0dd1f079a025e
Ernest (Tino) Deak <gordon.zar@gmail.com>
2021-08-31 23:05:20 +0200

    more shenanigans with rcs

--- updatev.sh
+++ updatev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-files=`find ./ -regex ".*\.sh\|.*\.pl\|.*/src/tpsh\|LICENSE\|README\.md"`
+files=`find ./ -regex ".*\.sh\|.*\.pl\|.*/src/tpsh\|\./LICENSE\|\./README\.md"`
 
 for f in $files; do
 	ci -m"$1" -f -l $f
